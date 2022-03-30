3b080db14523d89bc40c4691f3f7fe586b91f1ea
pounson <fakhradg@gmail.com>
2022-03-30 14:41:03 +0300

    first commit

---
+++ django_testing/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from rest_framework.test import APIClient
+from model_bakery import baker
+
+
+@pytest.fixture
+def api_client():
+    """Фикстура для клиента API"""
+    return APIClient()
+
+
+@pytest.fixture
+def course_factory():
+    def factory(**kwargs):
+        return baker.make('Course', **kwargs)
+    return factory
+
+
+@pytest.fixture
+def students_factory():
+    def factory(**kwargs):
+        return baker.make('Student', **kwargs)
+    return factory
